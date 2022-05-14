ec59495fbf6d7f07842293abf88db06375604d57
Martin Facchinello <martinfac@hotmail.com>
2022-05-14 15:17:47 -0300

    Cambio color bg y text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,14 +7,14 @@ import landscape03 from '../images/landscape03.jpg';
 
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-r from-indigo-300 to-blue-300 font-kanit">
+    <div className="flex flex-col min-h-screen bg-gradient-to-r from-yellow-200 to-orange-200 font-kanit">
       <h1 className="pt-8 text-4xl font-bold text-center text-grey-900 sm:text-5xl">CONTAME TU VIAJE</h1>
       <div className="">
-        <p className="px-6 py-6 text-sm font-semibold text-center text-red-900 sm:text-base">Este es un sitio colaborativo en el cual podrás encontrar experiencias y opiniones sobre distintos destinos turísticos. Por favor tener en cuenta que la información aquí compartida es sólo de carácter indicativo y corresponde a testimonios y experiencias particulares de distintos usuarios. <br/> <br/> <span className="text-base text-black sm:text-lg">Envíanos tus propias experiencias para que otros usuarios puedan conocer destinos potenciales y tomar la mejor decisión posible para su próximo viaje!</span></p>
+        <p className="px-6 py-6 text-sm font-semibold text-center text-orange-700 sm:text-base">Este es un sitio colaborativo en el cual podrás encontrar experiencias y opiniones sobre distintos destinos turísticos. Por favor tener en cuenta que la información aquí compartida es sólo de carácter indicativo y corresponde a testimonios y experiencias particulares de distintos usuarios. <br/> <br/> <span className="text-base text-red-700 sm:text-lg">Envíanos tus propias experiencias para que otros usuarios puedan conocer destinos potenciales y tomar la mejor decisión posible para su próximo viaje!</span></p>
       </div>
 
-      <div id="carouselExampleSlidesOnly" className="relative w-2/6 pb-10 left-1/3 carousel slide" data-bs-ride="carousel">
-        <div className="relative overflow-hidden carousel-inner">
+      <div id="carouselExampleSlidesOnly" className="relative w-2/6 pb-8 left-1/3 carousel slide" data-bs-ride="carousel">
+        <div className="relative overflow-hidden border rounded-md carousel-inner">
           <div className="relative float-left w-full carousel-item active">
             <img
               src={landscape01}
