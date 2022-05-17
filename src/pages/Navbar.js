3c35db9aebd2179d7ca92409de7f2ce8ae508588
Martin Facchinello <martinfac@hotmail.com>
2022-05-17 19:19:29 -0300

    Agregado Link a Home en Img

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,10 +1,11 @@
 import LogoAvion from '../images/LogoAvion.png'
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
   return (
     <div className="flex p-3 bg-gradient-to-r from-violet-700 to-rose-700">
-        <img src={LogoAvion} alt='LogoAvion' className="h-8 pl-2 sm:h-12"/>
+        <Link to="/"><img src={LogoAvion} alt='LogoAvion' className="pl-2 sm:h-12"/></Link>
         <h1 className="relative pl-8 text-3xl font-bold text-center md:pl-0 text-slate-100 font-macondo sm:text-5xl md:left-1/4 lg:left-1/3">CONTAME TU VIAJE</h1>
     </div>
   )
