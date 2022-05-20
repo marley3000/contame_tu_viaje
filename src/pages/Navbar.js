339db0ec05173ebf75ee17a29a8acd01fc0b49b3
Martin Facchinello <martinfac@hotmail.com>
2022-05-20 10:06:38 -0300

    Agregado de Inicio, enlace a Home

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="flex p-3 bg-gradient-to-r from-violet-700 to-rose-700">
-        <Link to="/"><img src={LogoAvion} alt='LogoAvion' className="pl-2 h-10 sm:h-12"/></Link>
-        <h1 className="relative pl-8 text-3xl font-bold text-center md:pl-0 text-slate-100 font-macondo sm:text-5xl md:left-1/4 lg:left-1/3">CONTAME TU VIAJE</h1>
+    <div className="flex justify-between items-center py-3 px-8 bg-gradient-to-r from-violet-700 to-rose-700">
+        <Link to="/"><img src={LogoAvion} alt='LogoAvion' className="sm:h-12 h-8"/></Link>
+        <h1 className="text-3xl font-bold text-center text-slate-100 font-macondo sm:text-5xl ">CONTAME TU VIAJE</h1>
+        <Link to="/"><h3 className="text-slate-100 font-kanit font-bold sm:text-base text-sm">Inicio</h3></Link>
     </div>
   )
 }
