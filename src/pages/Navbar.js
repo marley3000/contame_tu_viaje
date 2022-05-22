3d9217b52f5dbf94849a7b28785248b9de94e901
Martin Facchinello <martinfac@hotmail.com>
2022-05-22 17:38:28 -0300

    Cambio logo en navbar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,11 +1,11 @@
-import LogoAvion from '../images/LogoAvion.png'
+import LogoAvionSol from '../images/LogoAvionSol.png'
 import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center py-3 px-8 bg-gradient-to-r from-violet-700 to-rose-700">
-        <Link to="/"><img src={LogoAvion} alt='LogoAvion' className="sm:h-12 h-8"/></Link>
+    <div className="flex justify-between items-center px-8 bg-gradient-to-r from-violet-700 to-rose-700">
+        <Link to="/"><img src={LogoAvionSol} alt='LogoAvion' className="sm:h-20 h-12"/></Link>
         <h1 className="text-3xl font-bold text-center text-slate-100 font-macondo sm:text-5xl ">CONTAME TU VIAJE</h1>
         <Link to="/"><h3 className="text-slate-100 font-kanit font-bold sm:text-base text-sm">Inicio</h3></Link>
     </div>
