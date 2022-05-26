196958e0ddb957c7a9a1b753589ae6f3254e48bd
Martin Facchinello <martinfac@hotmail.com>
2022-05-26 18:10:59 -0300

    Agregado de action en form

--- src/pages/FindCountry.js
+++ src/pages/FindCountry.js
@@ -7,22 +7,22 @@ import avion from '../images/avion.png';
 
 const FindCountry = () => {
   return (
-    <div className="font-kanit flex flex-col justify-center items-center text-center">
-      <h2 className="text-blue-900 text-3xl font-bold m-4">BUSCA TU DESTINO!</h2>
-      <h5 className="text-violet-800 font-bold">Ingresa el país al que desearías viajar y obtiene las opiniones y experiencias de otros viajeros.</h5>
+    <div className="flex flex-col items-center justify-center text-center font-kanit">
+      <h2 className="m-4 text-3xl font-bold text-blue-900">BUSCA TU DESTINO!</h2>
+      <h5 className="font-bold text-violet-800">Ingresa el país al que desearías viajar y obtiene las opiniones y experiencias de otros viajeros.</h5>
       <div className="w-80 h-56 relative overflow-hidden bg-[#9AD7D8] rounded-md my-10">
-        <img src={paisaje_playa} alt="PaisajePlaya" className="absolute -bottom-2 w-full object-cover"/>
-        <img src={barco} alt="Barco" className="img-barco absolute w-1/4 -bottom-2 -right-12"/>
-        <img src={nube1} alt="Nube" className="img-nube absolute w-16 top-0 left-12"/>
-        <img src={nube2} alt="Nube" className="img-nube absolute w-9 top-5 left-32"/>
-        <img src={nube3} alt="Nube" className="img-nube absolute w-11 top-14 left-40"/>
+        <img src={paisaje_playa} alt="PaisajePlaya" className="absolute object-cover w-full -bottom-2"/>
+        <img src={barco} alt="Barco" className="absolute w-1/4 img-barco -bottom-2 -right-12"/>
+        <img src={nube1} alt="Nube" className="absolute top-0 w-16 img-nube left-12"/>
+        <img src={nube2} alt="Nube" className="absolute img-nube w-9 top-5 left-32"/>
+        <img src={nube3} alt="Nube" className="absolute img-nube w-11 top-14 left-40"/>
         <img src={avion} alt="Avion" className="img-avion absolute top-8 -left-12 w-[45%]"/>
       </div>
-      <form method="get" action="/verDestino/:destination" className="flex justify-center items-center">
-        <input type="text" placeholder="Ingresa un país" className="rounded-lg py-2 shadow-sm shadow-black text-violet-500 text-center bg-rose-50 mr-2" id="searchDestination" name="destination"/>
-        <button type="submit" className="p-2 hover:-translate-y-1 hover:translate-x-1 hover:shadow-md hover:shadow-black font-medium text-white bg-gradient-to-r from-violet-700 to-rose-700 rounded-md border-1">Buscar</button>
+      <form method="get" action="http://localhost:8080/api/verDestino/:destination" className="flex items-center justify-center">
+        <input type="text" placeholder="Ingresa un país" className="py-2 mr-2 text-center rounded-lg shadow-sm shadow-black text-violet-500 bg-rose-50" id="destination" name="destination"/>
+        <button type="submit" className="p-2 font-medium text-white rounded-md hover:-translate-y-1 hover:translate-x-1 hover:shadow-md hover:shadow-black bg-gradient-to-r from-violet-700 to-rose-700 border-1">Buscar</button>
       </form>
-      <p className="text-violet-800 text-sm absolute bottom-8">Por favor tener en cuenta que la información aquí compartida es sólo de carácter indicativo y corresponde a testimonios y experiencias particulares de distintos usuarios.</p>
+      <p className="absolute text-sm text-violet-800 bottom-8">Por favor tener en cuenta que la información aquí compartida es sólo de carácter indicativo y corresponde a testimonios y experiencias particulares de distintos usuarios.</p>
     </div>
   )
 }
--- src/pages/UploadTravel.js
+++ src/pages/UploadTravel.js
@@ -1,15 +1,14 @@
 
-
 const uploadTravel = () => {
   return (
     <div>
       <h2 className="m-4 text-3xl font-bold text-center text-blue-900 font-kanit">CARGA TU VIAJE!</h2>
       <h5 className="font-bold text-center font-kanit text-violet-800">Aquí puedes cargar tu experiencia de viaje. Recuerda que mientras más información incluyas, los viajeros podrán tomar mejores decisiones respecto a sus próximos destinos.</h5>
-      <form method="postTravel" action="/crear" className="grid gap-4 py-4 pl-4 mx-4 mt-8 rounded-md shadow-lg lg:grid-cols-2 sm:mx-10 lg:mx-20 font-kanit sm:pl-8 lg:pl-20 bg-gradient-to-r from-purple-200 to-orange-200">
+      <form method="post" action="http://localhost:8080/api/crear" className="grid gap-4 py-4 pl-4 mx-4 mt-8 rounded-md shadow-lg lg:grid-cols-2 sm:mx-10 lg:mx-20 font-kanit sm:pl-8 lg:pl-20 bg-gradient-to-r from-purple-200 to-orange-200">
         <label for="name" className="text-violet-900">Nombre: <input type="text" className="pl-2 rounded bg-rose-50 text-violet-500" id="name" name="name"/></label>
         <label for="destination" className="text-violet-900">País visitado: <input type="text" className="pl-2 rounded text-violet-500 w-36 bg-rose-50" id="destination" name="destination"/></label>
         <label for="number_travelers" className="text-violet-900">Cantidad de personas: <input type="number" className="w-12 pl-2 rounded text-violet-500 bg-rose-50" id="number_travelers" name="number_travelers"/></label>
-        <label for="duration_days" className="text-violet-900">Cantidad de días: <input type="number" className="w-12 pl-2 rounded text-violet-500" id="duration_days" name="duration_days bg-rose-50"/></label>
+        <label for="duration_days" className="text-violet-900">Cantidad de días: <input type="number" className="w-12 pl-2 rounded text-violet-500 bg-rose-50" id="duration_days" name="duration_days"/></label>
         <label for="valoration" className="text-violet-900">Valoración: <input type="number" className="w-12 pl-2 rounded text-violet-500 bg-rose-50" id="valoration" name="valoration"/></label>
         <br/>
         <label for="activities" className="text-violet-900">Actividades realizadas: <br/><textarea placeholder="Enumere aquí las actividades realizadas..." id="activities" name="activities" className="w-2/3 pl-2 rounded resize-none h-36 sm:w-4/5 text-violet-500 bg-rose-50"></textarea></label>
