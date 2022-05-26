04dc19ee03e17dd310db76915d610907ec1e79cd
Martin Facchinello <martinfac@hotmail.com>
2022-05-26 16:10:50 -0300

    Eliminación back

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {vistaUno} = require('../controller/controller.js')
-
-/* GET home page. */
-router.get('/', vistaUno);
-
-module.exports = router;
