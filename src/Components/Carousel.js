c44a5ef2433ab0abe96d83403be61b834622146a
Martin Facchinello <martinfac@hotmail.com>
2022-06-21 10:32:09 -0300

    Modif en width en modo responsive

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import landscape01 from '../images/landscape01.jpg';
 import landscape02 from '../images/landscape02.jpg';
 import landscape03 from '../images/landscape03.jpg';
@@ -5,7 +6,7 @@ import landscape03 from '../images/landscape03.jpg';
 
 const Carousel = () => {
     return (
-        <div id="carouselExampleSlidesOnly" className="relative w-2/6 pb-8 left-1/3 carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleSlidesOnly" className="relative w-1/2 sm:w-1/3 pb-8 left-1/4 sm:left-1/3 carousel slide" data-bs-ride="carousel">
             <div className="relative overflow-hidden border rounded-2xl carousel-inner">
                 <div className="relative float-left w-full carousel-item active">
                     <img
